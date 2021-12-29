617000d47e91fabd22c475d93b87028781960c60
AniketRaikwar_Vibrent <araikwar@vibrenthealth.com>
2021-12-29 14:47:59 +0530

    Aniket[ADDED]code for Pratice

---
+++ Hello/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <div>
+  <h1>Aniket pratice</h1>
+  <p>Here the fav. 5</p>
+  <ol>
+  <li>Dark</li>
+  <li>movie</li>
+  <li>Song</li>
+  <li>Series</li>
+  <li>Macth</li>
+  </ol>
+  </div>
+  ,document.getElementById("root")
+
+);
+  
+
